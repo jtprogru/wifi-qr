04849c73ff754acd50e6928f97e5bd9911beca54
Michael Savin <jtprogru@gmail.com>
2022-02-27 20:20:05 +0300

    Add comments in script
    Signed-off-by: Michael Savin <jtprogru@gmail.com>

--- wifi-qr.py
+++ wifi-qr.py
@@ -10,17 +10,20 @@ def main():
     WIFI_SSID = os.getenv("WIFI_SSID", None)
     WIFI_PROTOCOL = "WPA"
     WIFI_PASSWD = os.getenv("WIFI_PASSWD", None)
-
     WIFI_FILENAME = f"{WIFI_SSID}.png"
 
     if (WIFI_SSID is None) or (WIFI_PASSWD is None):
         print("ERROR - WIFI not set in environment")
+        print("Please see in README.md")
         sys.exit(1)
 
+    # About shis string please see in Wikipedia
+    # https://en.wikipedia.org/wiki/QR_code#Joining_a_Wi%E2%80%91Fi_network
     qrstr = f"WIFI:S:{WIFI_SSID};T:{WIFI_PROTOCOL};P:{WIFI_PASSWD};;"
 
     qr = pyqrcode.create(qrstr)
     qr.png(WIFI_FILENAME, scale=10)
+
     print(f"QR-code saved in {WIFI_FILENAME}")
     print("Please open this file in any image viewer")
     print("and scan this QR-code with your smartphone")
@@ -28,3 +31,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    sys.exit(0)
